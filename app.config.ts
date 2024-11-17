ba2d43e97dce0069cd00183fca4540e60cfe1358
gamersover <cmathking@gmail.com>
2024-11-17 22:05:19 +0800

    修改默认primary颜色

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ export default defineAppConfig({
       notifications: {
         // Show toasts at the top right of the screen
         position: 'top-0 bottom-auto left-1/2 -translate-x-1/2',
-      }
+      },
+      primary: 'blue'
     }
   })
