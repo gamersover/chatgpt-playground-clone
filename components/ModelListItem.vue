a5da301879ad3fac911b29d8b955a76f0e6a4f52
gamersover <cmathking@gmail.com>
2024-11-28 13:03:39 +0800

    添加复制模型配置按钮

--- components/ModelListItem.vue
+++ components/ModelListItem.vue
@@ -1,127 +1,155 @@
 <template>
-    <UButton
-        @click="emits('handleModelSelected')"
-        color="black"
-        variant="ghost"
-        :class="{'bg-gray-200 dark:bg-neutral-700': isSelected}"
-        class="hover:bg-gray-200 dark:hover:bg-neutral-700 w-full"
-        :padded="false"
-    >
-        <div class="flex justify-between w-full relative px-1">
-            <div
-                ref="name" :contenteditable="model.editable"
-                @blur="updateName"
-                @dblclick="changeName"
-                :class="{'dark:bg-neutral-800 bg-white': model.editable}"
-                class="overflow-hidden rounded-lg text-nowrap px-1 w-4/5 h-10 text-base font-normal flex items-center focus:border">
-                {{ model.showName }}
-            </div>
-            <UDropdown
-                :items="settings"
-                :popper="{placement: 'bottom-start'}"
-                :ui="{background: 'dark:bg-neutral-800'}"
-            >
-                <UButton
-                    v-show="isSelected"
-                    color="black"
-                    variant="ghost"
-                    :padded="false"
-                >
-                    <IconThreeDot size="24"/>
-                </UButton>
-            </UDropdown>
-        </div>
-        <UModal
-            v-model="isDelete"
-            :ui="{
-                rounded: 'rounded-2xl',
-                background: 'dark:bg-neutral-800',
-                overlay: {background: 'bg-black/50 dark:bg-black/80'}
-            }"
+  <UButton
+    @click="emits('handleModelSelected')"
+    color="black"
+    variant="ghost"
+    :class="{ 'bg-gray-200 dark:bg-neutral-700': isSelected }"
+    class="hover:bg-gray-200 dark:hover:bg-neutral-700 w-full"
+    :padded="false"
+  >
+    <div class="flex justify-between w-full relative px-1">
+      <div
+        ref="name"
+        :contenteditable="model.editable"
+        @blur="updateName"
+        @dblclick="changeName"
+        :class="{ 'dark:bg-neutral-800 bg-white': model.editable }"
+        class="overflow-hidden rounded-lg text-nowrap px-1 w-4/5 h-10 text-base font-normal flex items-center focus:border"
+      >
+        {{ model.showName }}
+      </div>
+      <UDropdown
+        :items="settings"
+        :popper="{ placement: 'bottom-start' }"
+        :ui="{ background: 'dark:bg-neutral-800' }"
+      >
+        <UButton
+          v-show="isSelected"
+          color="black"
+          variant="ghost"
+          :padded="false"
         >
-            <div class="flex flex-col p-4 gap-6">
-                <h1 class="text-lg font-medium">删除模型 "{{ model.showName }}"？</h1>
-                <p>该模型将被永久删除，无法恢复，确定继续吗？</p>
-                <div class="flex justify-end gap-2">
-                    <UButton
-                        label="取消"
-                        color="gray"
-                        :ui="{rounded: 'rounded-lg'}"
-                        class="text-base dark:bg-gray-300/20 dark:hover:bg-zinc-500"
-                        @click="isDelete=false"
-                    >
-                    </UButton>
-                    <UButton
-                        label="删除"
-                        color="red"
-                        :ui="{rounded: 'rounded-lg', variant: {solid: 'dark:bg-red-700 dark:hover:bg-red-600'}}"
-                        class="text-base dark:text-gray-200"
-                        @click="deleteModel"
-                    >
-                    </UButton>
-                </div>
-            </div>
-        </UModal>
-    </UButton>
+          <IconThreeDot size="24" />
+        </UButton>
+      </UDropdown>
+    </div>
+    <UModal
+      v-model="isDelete"
+      :ui="{
+        rounded: 'rounded-2xl',
+        background: 'dark:bg-neutral-800',
+        overlay: { background: 'bg-black/50 dark:bg-black/80' },
+      }"
+    >
+      <div class="flex flex-col p-4 gap-6">
+        <h1 class="text-lg font-medium">删除模型 "{{ model.showName }}"？</h1>
+        <p>该模型将被永久删除，无法恢复，确定继续吗？</p>
+        <div class="flex justify-end gap-2">
+          <UButton
+            label="取消"
+            color="gray"
+            :ui="{ rounded: 'rounded-lg' }"
+            class="text-base dark:bg-gray-300/20 dark:hover:bg-zinc-500"
+            @click="isDelete = false"
+          >
+          </UButton>
+          <UButton
+            label="删除"
+            color="red"
+            :ui="{
+              rounded: 'rounded-lg',
+              variant: { solid: 'dark:bg-red-700 dark:hover:bg-red-600' },
+            }"
+            class="text-base dark:text-gray-200"
+            @click="deleteModel"
+          >
+          </UButton>
+        </div>
+      </div>
+    </UModal>
+  </UButton>
 </template>
 
 <script setup>
-const props = defineProps(['model', 'isSelected'])
-const emits = defineEmits(['handleModelSelected', 'removeModel'])
+const props = defineProps(["model", "isSelected"]);
+const emits = defineEmits([
+  "handleModelSelected",
+  "removeModel",
+  "duplicateModel",
+]);
 
-const name = ref(null)
-const isDelete = ref(false)
-
+const name = ref(null);
+const isDelete = ref(false);
 
 async function changeName() {
-    props.model.editable = true
-    await nextTick()
-    const el = name.value
-    el.focus()
-    if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
-        var range = document.createRange();
-        range.selectNodeContents(el);
-        range.collapse(false);
-        var sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-      } else if (typeof document.body.createTextRange != "undefined") {
-        var textRange = document.body.createTextRange();
-        textRange.moveToElementText(el);
-        textRange.collapse(false);
-        textRange.select();
-      }
-    props.model.showPopup = false
+  props.model.editable = true;
+  await nextTick();
+  const el = name.value;
+  el.focus();
+  if (
+    typeof window.getSelection != "undefined" &&
+    typeof document.createRange != "undefined"
+  ) {
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    range.collapse(false);
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+  } else if (typeof document.body.createTextRange != "undefined") {
+    var textRange = document.body.createTextRange();
+    textRange.moveToElementText(el);
+    textRange.collapse(false);
+    textRange.select();
+  }
+  props.model.showPopup = false;
 }
 
 function updateName() {
-    props.model.editable = false
-    props.model.showName = name.value.textContent
+  props.model.editable = false;
+  props.model.showName = name.value.textContent;
 }
 
 function deleteModel() {
-    emits('removeModel')
-    isDelete.value = false
-    props.model.showPopup = false
+  emits("removeModel");
+  isDelete.value = false;
+  props.model.showPopup = false;
+}
+
+function duplicateModel(e) {
+  e.stopPropagation();
+  emits("duplicateModel");
 }
 
 const settings = [
-    [{
-        label: "重命名",
-        click: () => {
-            changeName()
-        },
-        icon: "i-heroicons-pencil-square",
-        class: "text-base dark:text-gray-200",
-        iconClass: "dark:text-gray-200"
-    },{
-        label: "删除",
-        click: () => {
-            isDelete.value = true
-        },
-        icon: "i-heroicons-trash",
-        class: "text-red-500 dark:text-red-500 text-base",
-        iconClass: "text-red-500 dark:text-red-500"
-    }]
-]
+  [
+    {
+      label: "重命名",
+      click: () => {
+        changeName();
+      },
+      icon: "i-heroicons-pencil-square",
+      class: "text-base dark:text-gray-200",
+      iconClass: "dark:text-gray-200",
+    },
+    {
+      label: "复制",
+      click: (e) => {
+        duplicateModel(e);
+      },
+      icon: "i-heroicons-document-duplicate",
+      class: "text-base dark:text-gray-200",
+      iconClass: "dark:text-gray-200",
+    },
+    {
+      label: "删除",
+      click: () => {
+        isDelete.value = true;
+      },
+      icon: "i-heroicons-trash",
+      class: "text-red-500 dark:text-red-500 text-base",
+      iconClass: "text-red-500 dark:text-red-500",
+    },
+  ],
+];
 </script>
--- components/ModelSetModal.vue
+++ components/ModelSetModal.vue
@@ -1,247 +1,288 @@
 <template>
-<div class="flex flex-col rounded-2xl dark:text-gray-100">
-    <div class="h-16 flex justify-between items-center border-b border-black/10 dark:border-white/10 px-4">
-        <h1 class="font-medium text-2xl">设置</h1>
-        <UButton
-            @click="emits('closeModal')"
-            variant="ghost"
-            color="black"
-            class="text-gray-700 hover:text-black dark:text-gray-300 dark:hover:text-gray-100"
-        >
-            <IconClose size="24"/>
-        </UButton>
+  <div class="flex flex-col rounded-2xl dark:text-gray-100">
+    <div
+      class="h-16 flex justify-between items-center border-b border-black/10 dark:border-white/10 px-4"
+    >
+      <h1 class="font-medium text-2xl">设置</h1>
+      <UButton
+        @click="emits('closeModal')"
+        variant="ghost"
+        color="black"
+        class="text-gray-700 hover:text-black dark:text-gray-300 dark:hover:text-gray-100"
+      >
+        <IconClose size="24" />
+      </UButton>
     </div>
     <div class="flex flex-1">
-        <div class="flex flex-col px-4 py-2">
-            <UButton
-                @click="handleNewModel"
-                :class="{'bg-gray-200 dark:bg-neutral-700': isAddPanel}"
-                class="hover:bg-gray-200 dark:hover:bg-neutral-700 font-normal text-base w-full p-2 text-start"
-                label="新建"
-                variant="ghost"
-                color="black"
-            >
-            </UButton>
-            <ul class="flex flex-col h-[300px] overflow-y-auto w-[200px]">
-                <li v-for="(model, index) in models" :key="index">
-                    <ModelListItem :isSelected="currentModel===index" @HandleModelSelected="handleModelSelected(index)" @RemoveModel="removeModel" :model="model"/>
-                </li>
-            </ul>
+      <div class="flex flex-col px-4 py-2">
+        <UButton
+          @click="handleNewModel"
+          :class="{ 'bg-gray-200 dark:bg-neutral-700': isAddPanel }"
+          class="hover:bg-gray-200 dark:hover:bg-neutral-700 font-normal text-base w-full p-2 text-start"
+          label="新建"
+          variant="ghost"
+          color="black"
+        >
+        </UButton>
+        <ul class="flex flex-col h-[300px] overflow-y-auto w-[200px]">
+          <li v-for="(model, index) in models" :key="index">
+            <ModelListItem
+              :isSelected="currentModel === index"
+              @handle-model-selected="handleModelSelected(index)"
+              @duplicate-model="duplicateModel(model)"
+              @remove-model="removeModel(index)"
+              :model="model"
+            />
+          </li>
+        </ul>
+      </div>
+      <div class="flex flex-col flex-1 justify-around items-center gap-6 p-4">
+        <div class="flex flex-col gap-2 w-full">
+          <p><span class="text-red-500">*</span> Model UID</p>
+          <UInput
+            v-model.trim="modelConfig.model"
+            :ui="{
+              base: 'w-full h-[48px]',
+              variant: {
+                outline:
+                  'ring-gray-300 hover:ring-gray-400 dark:ring-gray-600 dark:hover:ring-gray-500',
+              },
+            }"
+            color="blue"
+            size="xl"
+          >
+          </UInput>
         </div>
-        <div class="flex flex-col flex-1 justify-around items-center gap-6 p-4">
-            <div class="flex flex-col gap-2 w-full">
-                <p><span class="text-red-500">*</span> Model UID</p>
-                <UInput
-                    v-model.trim="modelConfig.model"
-                    :ui="{
-                        'base': 'w-full h-[48px]',
-                        'variant': {'outline': 'ring-gray-300 hover:ring-gray-400 dark:ring-gray-600 dark:hover:ring-gray-500'},
-                    }"
-                    color="blue"
-                    size="xl"
-                >
-                </UInput>
-            </div>
-            <div class="flex flex-col gap-2 w-full">
-                <p><span class="text-red-500">*</span> Base URL</p>
-                <UInput
-                    v-model.trim="modelConfig.url"
-                    :ui="{
-                        'base': 'w-full h-[48px]',
-                        'variant': {'outline': 'ring-gray-300 hover:ring-gray-400 dark:ring-gray-600 dark:hover:ring-gray-500'},
-                    }"
-                    color="blue"
-                    size="xl"
-                >
-                </UInput>
-            </div>
-            <div class="flex flex-col gap-2 w-full">
-                <p>Secret Key <span class="font-light">可选</span></p>
-                <UInput
-                    v-model.trim="modelConfig.sk"
-                    :ui="{
-                        'base': 'w-full h-[48px]',
-                        'variant': {'outline': 'ring-gray-300 hover:ring-gray-400 dark:ring-gray-600 dark:hover:ring-gray-500'},
-                    }"
-                    color="blue"
-                    size="xl"
-                    type="password"
-                >
-                </UInput>
-            </div>
-            <div class="flex items-center gap-2 self-end">
-                <UButton
-                    @click="testModel"
-                    label="测试"
-                    size="xl"
-                    :disabled="!testButtonEnable"
-                    :loading="testStatus==='testing'"
-                    :ui="{variant: {solid: 'bg-[#10a37f] hover:bg-[#1a7f64] dark:bg-[#1a7f64] dark:hover:bg-[#1a7f64]/80'}}"
-                    class='px-3 py-1.5 dark:text-gray-200 disabled:bg-primary-600/50 dark:disabled:bg-[#1a7f64]/30'
-                >
-                </UButton>
-                <UButton
-                   @click="addOrUpdateModel"
-                   :label="updateButtonEnable ? '更新' : ' 添加'"
-                   size="xl"
-                   :disabled="!canAddOrUpdate"
-                   color="blue"
-                   :ui="{variant: {solid: 'dark:bg-blue-800 dark:hover:bg-blue-800/80 disabled:bg-blue-600/50 dark:disabled:bg-blue-700/30'}}"
-                   class="px-3 py-1.5 dark:text-gray-200"
-                >
-                </UButton>
-            </div>
+        <div class="flex flex-col gap-2 w-full">
+          <p><span class="text-red-500">*</span> Base URL</p>
+          <UInput
+            v-model.trim="modelConfig.url"
+            :ui="{
+              base: 'w-full h-[48px]',
+              variant: {
+                outline:
+                  'ring-gray-300 hover:ring-gray-400 dark:ring-gray-600 dark:hover:ring-gray-500',
+              },
+            }"
+            color="blue"
+            size="xl"
+          >
+          </UInput>
         </div>
+        <div class="flex flex-col gap-2 w-full">
+          <p>Secret Key <span class="font-light">可选</span></p>
+          <UInput
+            v-model.trim="modelConfig.sk"
+            :ui="{
+              base: 'w-full h-[48px]',
+              variant: {
+                outline:
+                  'ring-gray-300 hover:ring-gray-400 dark:ring-gray-600 dark:hover:ring-gray-500',
+              },
+            }"
+            color="blue"
+            size="xl"
+            type="password"
+          >
+          </UInput>
+        </div>
+        <div class="flex items-center gap-2 self-end">
+          <UButton
+            @click="testModel"
+            label="测试"
+            size="xl"
+            :disabled="!testButtonEnable"
+            :loading="testStatus === 'testing'"
+            :ui="{
+              variant: {
+                solid:
+                  'bg-[#10a37f] hover:bg-[#1a7f64] dark:bg-[#1a7f64] dark:hover:bg-[#1a7f64]/80',
+              },
+            }"
+            class="px-3 py-1.5 dark:text-gray-200 disabled:bg-[#10a37f] dark:disabled:bg-[#1a7f64]/30"
+          >
+          </UButton>
+          <UButton
+            @click="addOrUpdateModel"
+            :label="updateButtonEnable ? '更新' : ' 添加'"
+            size="xl"
+            :disabled="!canAddOrUpdate"
+            color="blue"
+            :ui="{
+              variant: {
+                solid:
+                  'dark:bg-blue-800 dark:hover:bg-blue-800/80 disabled:bg-blue-600/50 dark:disabled:bg-blue-700/30',
+              },
+            }"
+            class="px-3 py-1.5 dark:text-gray-200"
+          >
+          </UButton>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
-
 <script setup>
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
-const toast = useToast()
-const emits = defineEmits(["closeModal"])
-const props = defineProps(['models'])
-
+const toast = useToast();
+const emits = defineEmits(["closeModal"]);
+const props = defineProps(["models"]);
 
 const modelConfig = ref({
-    model: null,
-    url: null,
-    sk: null
-})
+  model: null,
+  url: null,
+  sk: null,
+});
 
-const isAddPanel = ref(true)
-const currentModel = ref(null)
+const isAddPanel = ref(true);
+const currentModel = ref(null);
 
-const canTest = ref(false)
-const testStatus = ref(null)
-const canAddOrUpdate = ref(false)
-
+const canTest = ref(false);
+const testStatus = ref(null);
+const canAddOrUpdate = ref(false);
 
 const testButtonEnable = computed(() => {
-    return canTest.value && testStatus.value!=='testing'
-})
+  return canTest.value && testStatus.value !== "testing";
+});
 const updateButtonEnable = computed(() => {
-    return currentModel.value!==null && modelConfig.value.model!==null && modelConfig.value.url!==null
-})
+  return (
+    currentModel.value !== null &&
+    modelConfig.value.model !== null &&
+    modelConfig.value.url !== null
+  );
+});
 
-watch(modelConfig, () => {
-    canTest.value = (
-        modelConfig.value.model!==null && modelConfig.value.url!==null
-        && modelConfig.value.model.length > 0 && modelConfig.value.url.length > 0
-    )
-}, {deep: true})
-
+watch(
+  modelConfig,
+  () => {
+    canTest.value =
+      modelConfig.value.model !== null &&
+      modelConfig.value.url !== null &&
+      modelConfig.value.model.length > 0 &&
+      modelConfig.value.url.length > 0;
+  },
+  { deep: true }
+);
 
 function handleNewModel() {
-    isAddPanel.value = true
-    currentModel.value = null
-    modelConfig.value = {
-        model: null,
-        url: null,
-        sk: null
-    }
-    canAddOrUpdate.value = false
-    testStatus.value = null
+  isAddPanel.value = true;
+  currentModel.value = null;
+  modelConfig.value = {
+    model: null,
+    url: null,
+    sk: null,
+  };
+  canAddOrUpdate.value = false;
+  testStatus.value = null;
 }
 
 function handleModelSelected(index) {
-    isAddPanel.value = false
-    currentModel.value = index
-    modelConfig.value = {
-        model: props.models[index].modelName,
-        url: props.models[index].baseURL,
-        sk: props.models[index].apiKey
-    }
-    canAddOrUpdate.value = false
-    testStatus.value = null
+  isAddPanel.value = false;
+  currentModel.value = index;
+  modelConfig.value = {
+    model: props.models[index].modelName,
+    url: props.models[index].baseURL,
+    sk: props.models[index].apiKey,
+  };
+  canAddOrUpdate.value = false;
+  testStatus.value = null;
 }
 
 function addOrUpdateModel() {
-    if (updateButtonEnable.value) {
-        props.models[currentModel.value].modelName = modelConfig.value.model
-        props.models[currentModel.value].baseURL = modelConfig.value.url
-        props.models[currentModel.value].apiKey = modelConfig.value.sk
-        toast.add({
-            title: '成功',
-            description: '模型已更新',
-            color: 'green',
-            icon: 'i-heroicons-check-circle-20-solid',
-        })
-    } else {
-        props.models.push({
-            id: uuidv4(),
-            showName: modelConfig.value.model,
-            modelName: modelConfig.value.model,
-            baseURL: modelConfig.value.url,
-            apiKey: modelConfig.value.sk,
-            editable: false,
-            showPopup: false
-        })
-        currentModel.value = props.models.length - 1
-        isAddPanel.value = false
-        toast.add({
-            title: '成功',
-            description: '模型已添加',
-            color: 'green',
-            icon: 'i-heroicons-check-circle-20-solid',
-        })
-    }
-    canAddOrUpdate.value = false
+  if (updateButtonEnable.value) {
+    props.models[currentModel.value].modelName = modelConfig.value.model;
+    props.models[currentModel.value].baseURL = modelConfig.value.url;
+    props.models[currentModel.value].apiKey = modelConfig.value.sk;
+    toast.add({
+      title: "成功",
+      description: "模型已更新",
+      color: "green",
+      icon: "i-heroicons-check-circle-20-solid",
+    });
+  } else {
+    props.models.push({
+      id: uuidv4(),
+      showName: modelConfig.value.model,
+      modelName: modelConfig.value.model,
+      baseURL: modelConfig.value.url,
+      apiKey: modelConfig.value.sk,
+      editable: false,
+      showPopup: false,
+    });
+    currentModel.value = props.models.length - 1;
+    isAddPanel.value = false;
+    toast.add({
+      title: "成功",
+      description: "模型已添加",
+      color: "green",
+      icon: "i-heroicons-check-circle-20-solid",
+    });
+  }
+  canAddOrUpdate.value = false;
 }
 
-function removeModel() {
-    props.models.splice(currentModel.value, 1)
-    handleNewModel()
+function duplicateModel(config) {
+  handleNewModel();
+  modelConfig.value = {
+    model: config.modelName,
+    url: config.baseURL,
+    sk: config.apiKey,
+  };
+}
+
+function removeModel(index) {
+  props.models.splice(index, 1);
+  handleNewModel();
 }
 
 async function testModel() {
-    testStatus.value = "testing"
+  testStatus.value = "testing";
 
-    try {
-        const rawResponse = await fetch("/api/openai", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                ...modelConfig.value,
-                messages: [{role: "user", content: "Hello!"}],
-                max_tokens: 1,
-                temperature: 0.7,
-                stop: [],
-                stream: false
-            })
-        })
-        const content = await rawResponse.json()
+  try {
+    const rawResponse = await fetch("/api/openai", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...modelConfig.value,
+        messages: [{ role: "user", content: "Hello!" }],
+        max_tokens: 1,
+        temperature: 0.7,
+        stop: [],
+        stream: false,
+      }),
+    });
+    const content = await rawResponse.json();
 
-        if (!rawResponse.ok || (content.object === "error" || content.error)) {
-            toast.add({
-                description: content.message || content.error.message,
-                title: '失败',
-                color: "red",
-                icon: "i-heroicons-exclamation-circle-20-solid",
-            })
-        } else {
-            toast.add({
-                title: '成功',
-                description: '模型测试通过',
-                color: 'green',
-                icon: 'i-heroicons-check-circle-20-solid',
-            })
-            canAddOrUpdate.value = true
-        }
-        testStatus.value = "finished"
-    } catch (e) {
-        toast.add({
-            title: '失败',
-            description: e.toString(),
-            color: 'red',
-            icon: 'i-heroicons-exclamation-circle-20-solid',
-        })
-        testStatus.value = "finished"
+    if (!rawResponse.ok || content.object === "error" || content.error) {
+      toast.add({
+        description: content.message || content.error.message,
+        title: "失败",
+        color: "red",
+        icon: "i-heroicons-exclamation-circle-20-solid",
+      });
+    } else {
+      toast.add({
+        title: "成功",
+        description: "模型测试通过",
+        color: "green",
+        icon: "i-heroicons-check-circle-20-solid",
+      });
+      canAddOrUpdate.value = true;
     }
+    testStatus.value = "finished";
+  } catch (e) {
+    toast.add({
+      title: "失败",
+      description: e.toString(),
+      color: "red",
+      icon: "i-heroicons-exclamation-circle-20-solid",
+    });
+    testStatus.value = "finished";
+  }
 }
 </script>
